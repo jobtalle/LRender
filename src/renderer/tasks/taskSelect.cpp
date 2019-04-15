74b8d9256fcea98caa734aeb3ee0ea5a41523f70
jobtalle <jobtalle@hotmail.com>
2019-04-15 16:34:24 +0200

    Removed redundant functionality from select task

--- src/renderer/tasks/taskSelect.cpp
+++ src/renderer/tasks/taskSelect.cpp
@@ -5,8 +5,7 @@ using namespace LRender;
 
 Renderer::Task::Select::Select(float x, float y) :
 	x(x),
-	y(y),
-	selectedValue(selected.get_future()) {
+	y(y) {
 	
 }
 
@@ -16,8 +15,4 @@ void Renderer::Task::Select::perform(Renderer &renderer) {
 	renderer.render(pass);
 	renderer.setSelected(pass.sample(x, y));
 	renderer.setMode(Renderer::Mode::DEFAULT);
-}
-
-int Renderer::Task::Select::getSelected() {
-	return selectedValue.get();
 }
